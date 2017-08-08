0e5beb0ae0e6d3a7df36c21b03efd1c3586ee200
Aaron Eugene Kester <aaron.opta@yahoo.com>
2017-08-08 13:50:16 -0700

    ready for file reader

---
+++ src/markdown.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+module.exports = {
+  reader: function(file) {
+    console.log(file);
+    return ''
+  }
+}
--- test/markdown-test.js
+++ test/markdown-test.js
@@ -1,8 +1,16 @@
 const chai = require('chai')
 const assert = chai.assert
+const markdown = require('../src/markdown.js')
+const path = require('path')
 
-describe('test', () => {
-  it('should do this', () => {
-    
+const file1 =
+
+
+let result = markdown.reader(file1)
+
+describe('#reader', () => {
+  let expected = '# Header'
+  it('should print file', () => {
+    assert.equal(result, expected)
   })
 })
